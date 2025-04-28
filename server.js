0740ef06de8d9fdecd96c56d28359c72451b26f2
wildwoodpool <wildwoodpoolautomation@gmail.com>
2025-04-28 11:45:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch');
+const fetch = require('node-fetch');  // Correct import
 
 const app = express();
 app.use(cors());
